6bd514f06c725df3cf5737cf8f800f47644dd1e4
jessebarreto <jesseh.barreto@gmail.com>
2016-11-05 18:47:18 -0200

    testing..

---
+++ ardrone_uav.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ardrone_uav.cpp
+// Author      : Jesseh
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "begin" << endl; // prints begin
+	return 0;
+}
